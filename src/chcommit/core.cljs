5cb358e2d906b4427b289c4aec5a06593749e7e2
Jay <jayzawrotny@gmail.com>
2017-10-24 01:56:09 -0400

    Used the thread operator and made a request

--- src/chcommit/core.cljs
+++ src/chcommit/core.cljs
@@ -1,9 +1,15 @@
 (ns chcommit.core
-  (:require [cljs.nodejs :as nodejs]))
+  (:require [cljs.nodejs :as nodejs]
+            ["http" :as http]))
 
 (nodejs/enable-util-print!)
 
+(require '["request" :as request]
+         '["highland" :as highland])
 (defn -main [& args]
-  (println "Hello world!"))
+  (-> (request "https://httpbin.org/ip")
+      (.pipe (highland))
+      (.each (fn [output]
+                (print (.toString output "utf8"))))))
 
 (set! *main-cli-fn* -main)
